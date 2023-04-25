32dae140a3c66ebf8098d33a500a7c532e5730c8
canngos <baturlarcan@gmail.com>
2023-04-25 15:45:18 +0300

    Inital project

---
+++ src/test/java/com/back2261/communityservice/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2261.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
